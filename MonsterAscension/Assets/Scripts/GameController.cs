09fb2ecd160a4ced6b915c2e95978066a08c5c46
Edward Gutierrez <Edwardgutierrez1996@gmail.com>
2018-11-03 00:34:07 -0400

    Rocks
    work on rocks

---
+++ MonsterAscension/Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject hazard;
+    public float spawnHeight;
+    public float spawnDistance;
+    public int lanes;
+    public float spawnWait;
+    public float startWait;
+    
+	void Start () {
+        StartCoroutine(SpawnHazards());
+	}
+	
+    IEnumerator SpawnHazards ()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            int lane = Mathf.FloorToInt(Random.Range(0, lanes));
+            Debug.Log(lane);
+            float angle = (2 * Mathf.PI / lanes) * lane;
+            float x = spawnDistance * Mathf.Cos(angle);
+            float z = spawnDistance * Mathf.Sin(angle);
+            Vector3 spawnPosition = new Vector3(x, spawnHeight, z);
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(hazard, spawnPosition, spawnRotation);
+            yield return new WaitForSeconds(spawnWait);
+        }
+    }
+}
---
+++ MonsterAscension/Assets/Scripts/RockGravity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockGravity : MonoBehaviour {
+
+    public float baseSpeed;
+    public float[] speedMultipliers = new float[4];
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
