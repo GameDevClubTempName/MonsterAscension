56cac608dc51d4aaad74db57f8bf71214a17c07c
Tina <18502544+tinam-26@users.noreply.github.com>
2018-11-03 18:18:20 -0400

    for when we get colision, the slider will update

--- MonsterAscension/Assets/Scripts/SliderController.cs
+++ MonsterAscension/Assets/Scripts/SliderController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SliderController : MonoBehaviour {
-
+	public Slider Slider; 
+	public int startingMonster = 0; 
+	public int currentMonster = 0; 
+	public int currentLvlMonster = 5; 
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +14,23 @@ public class SliderController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if(monsterCollected){
+			currentMonster ++; 
+			Silder.value = currentMonster; 
+			if(monsterCollected == currentLvlMonster){
+				currentMonster += 5; 
+			}
+		}
 		
+
+		if(hitByBoulder){
+			currentMonster --; 
+			Slider.value = currentMonster; 
+			if(monsterCollected == 0){
+				//ded
+			}
+		}
+
+
 	}
 }
