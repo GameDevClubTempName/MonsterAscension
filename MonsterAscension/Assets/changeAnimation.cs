a6c7e6ecec83697aee3c81c5252c0b76f3b3f683
Tina <18502544+tinam-26@users.noreply.github.com>
2018-11-03 21:55:54 -0400

    animator tree is a thing now

---
+++ MonsterAscension/Assets/changeAnimation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeAnimation : MonoBehaviour {
+	public Animation Animation;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
