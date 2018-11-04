08223b78700c06091262ebebfac5f2d50e756857
Tina <18502544+tinam-26@users.noreply.github.com>
2018-11-03 22:18:59 -0400

    *i have no idea*

--- MonsterAscension/Assets/changeAnimation.cs
+++ MonsterAscension/Assets/changeAnimation.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class changeAnimation : MonoBehaviour {
-	public Animation Animation;
+	public int characterlevel; 
+	public AnimationClip[] playerAnimation1;
+	public AnimationClip[] playerAnimation2;
+	public AnimationClip[] playerAnimation3;
+	public AnimationClip[] playerAnimation4;
+	public AnimatorOverrideController ZombieAnimator; 
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +16,14 @@ public class changeAnimation : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(characterlevel == 1){
+			ZombieAnimator[playerAnimation1];
+		}else if (characterlevel ==2){
+			ZombieAnimator[playerAnimation2];
+		}else if(characterlevel == 3){
+			ZombieAnimator[playerAnimation3];
+		}else if(characterlevel == 4){
+			ZombieAnimator[playerAnimation4];
+		}
 	}
 }
