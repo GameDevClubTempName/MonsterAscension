de649acd422e477faaa21126a5cf0311cb2132f6
Tina <18502544+tinam-26@users.noreply.github.com>
2018-11-03 20:21:11 -0400

    added level up and game over images

---
+++ MonsterAscension/Assets/_Scripts/GameOverController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class GameOverController : MonoBehaviour {
+	public bool gameOver; 
+	public Image GameOverImage; 
+	// Use this for initialization
+	void Start () {
+		GameOverImage.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(gameOver){
+			GameOverImage.enabled = true; 
+		}
+	}
+}
